a430a71db816324eac4f2f123521d3faf384c2fa
José Gabriel González Pérez <jgabrielsinner10@gmail.com>
2014-06-01 20:45:09 -0430

    Auto-disable debugtoolbar depend of response type
    This is a feature based on #58 Pull Request.
    
    With this new feature, the debug toolbar will automatically disable itself whenever yii serves content that isn't on the white list.
    
    This is useful for web services producing pdf, xml, csv, or json output, for serving binary data like images, ...
    
    This should automatically solve issue #18 and maybe also issue #43 (depending on why one wants to disable the toolbar for certain routes).
    
    And is better than #58 Pull Request because add validations of Whitelist types in the onBeginRequest method.

--- yii-debug-toolbar/YiiDebugToolbarRoute.php
+++ yii-debug-toolbar/YiiDebugToolbarRoute.php
@@ -15,186 +15,190 @@
  * @package YiiDebugToolbar
  * @since 1.1.7
  */
-class YiiDebugToolbarRoute extends CLogRoute
-{
+class YiiDebugToolbarRoute extends CLogRoute {
 
     private $_panels = array(
-        'YiiDebugToolbarPanelServer',
+        //'YiiDebugToolbarPanelServer',
         'YiiDebugToolbarPanelRequest',
         'YiiDebugToolbarPanelSettings',
-        'YiiDebugToolbarPanelViewsRendering',
+        'YiiDebugToolbarPanelViews',
         'YiiDebugToolbarPanelSql',
         'YiiDebugToolbarPanelLogging',
     );
 
-    /* The filters are given in an array, each filter being:
+    /**
+     * The filters are given in an array, each filter being:
      * - a normal IP (192.168.0.10 or '::1')
      * - an incomplete IP (192.168.0.* or 192.168.0.)
      * - a CIDR mask (192.168.0.0/24)
      * - "*" for everything.
      */
-    public $ipFilters=array('127.0.0.1','::1');
-    
-    /**
-     * This is a list of paths to extra panels.
-     * Example:
-     * 'additionalPanels' => array(
-     *    'append:application.extensions.debug-panels.newPanel', // added panel as last
-     *    'prepend:application.extensions.debug-panels.newPanel2', // added panel as first
-     *    'application.extensions.debug-panels.newPanel3' // added panel as last
-     * )
-     * @var array
-     */
-    public $additionalPanels = array();
+    public $ipFilters = array('127.0.0.1', '::1');
 
     /**
-     * If true, then after reloading the page will open the current panel.
-     * @var bool
+     * Whitelist for response content types. DebugToolbarRoute won't write any
+     * output if the server generates output that isn't listed here (json, xml,
+     * files, ...)
+     * @var array of content type strings (in lower case)
      */
-    public $openLastPanel = true;
-
+    public $contentTypeWhitelist = array(
+        // Yii framework doesn't seem to send content-type header by default.
+        '',
+        'text/html',
+        'application/xhtml+xml',
+    );
     private $_toolbarWidget,
             $_startTime,
             $_endTime;
-
-
     private $_proxyMap = array(
         'viewRenderer' => 'YiiDebugViewRenderer'
     );
 
-    public function setPanels(array $pannels)
-    {
+    public function setPanels(array $pannels) {
         $selfPanels = array_fill_keys($this->_panels, array());
         $this->_panels = array_merge($selfPanels, $pannels);
     }
 
-    public function getPanels()
-    {
+    public function getPanels() {
         return $this->_panels;
     }
 
-    public function getStartTime()
-    {
+    public function getStartTime() {
         return $this->_startTime;
     }
 
-    public function getEndTime()
-    {
+    public function getEndTime() {
         return $this->_endTime;
     }
 
-    public function getLoadTime()
-    {
-        return ($this->endTime-$this->startTime);
+    public function getLoadTime() {
+        return ($this->endTime - $this->startTime);
     }
 
-    protected function getToolbarWidget()
-    {
-        if (null === $this->_toolbarWidget)
-        {
+    protected function getToolbarWidget() {
+        if (null === $this->_toolbarWidget) {
             $this->_toolbarWidget = Yii::createComponent(array(
-                'class'=>'YiiDebugToolbar',
-                'panels'=> $this->panels
+                        'class' => 'YiiDebugToolbar',
+                        'panels' => $this->panels
             ), $this);
-
-            
-            if(!empty($this->additionalPanels) and is_array($this->additionalPanels))
-            {
-                foreach($this->additionalPanels as $panel)
-                {
-                    $pos = 'append';
-                    if(($dotpos=strpos($panel, ':'))!==false){
-                            $pos = substr($panel, 0, $dotpos) == 'prepend' ? 'prepend' : 'append';
-                            $panel = substr($panel, $dotpos+1);
-                    }
-                    $this->_toolbarWidget->{$pos.'Panel'}($panel);
-                }
-            }
         }
         return $this->_toolbarWidget;
     }
 
-    public function init()
-    {
-        $this->_startTime=microtime(true);
+    public function init() {
+        Yii::app()->controllerMap = array_merge(array(
+            'debug' => array(
+                'class' => 'YiiDebugController'
+            )
+                ), Yii::app()->controllerMap);
 
-        parent::init();
+        Yii::setPathOfAlias('yii-debug-toolbar', dirname(__FILE__));
 
-        $this->enabled && $this->enabled = ($this->allowIp(Yii::app()->request->userHostAddress)
-                && !Yii::app()->getRequest()->getIsAjaxRequest() && (Yii::app() instanceof CWebApplication));
+        Yii::app()->setImport(array(
+            'yii-debug-toolbar.*'
+        ));
 
-        if ($this->enabled)
-        {
+        $route = Yii::app()->getUrlManager()->parseUrl(Yii::app()->getRequest());
+
+        $this->enabled = strpos(trim($route, '/'), 'debug') !== 0;
+
+        $this->enabled && $this->enabled = ($this->allowIp(Yii::app()->request->userHostAddress) && !Yii::app()->getRequest()->getIsAjaxRequest() && (Yii::app() instanceof CWebApplication)) && $this->checkContentTypeWhitelist();
+
+        if ($this->enabled) {
             Yii::app()->attachEventHandler('onBeginRequest', array($this, 'onBeginRequest'));
             Yii::app()->attachEventHandler('onEndRequest', array($this, 'onEndRequest'));
-            Yii::setPathOfAlias('yii-debug-toolbar', dirname(__FILE__));
-            Yii::app()->setImport(array(
-                'yii-debug-toolbar.*',
-                'yii-debug-toolbar.components.*'
-            ));
+
             $this->categories = '';
-            $this->levels='';
+            $this->levels = '';
         }
+
+        $this->_startTime = microtime(true);
+
+        parent::init();
     }
 
-    protected function onBeginRequest(CEvent $event)
-    {
-        $this->initComponents();
+    protected function onBeginRequest(CEvent $event) {
 
-        $this->getToolbarWidget()
-             ->init();
+        $this->enabled = $this->enabled && $this->checkContentTypeWhitelist();
+
+        if ($this->enabled) {
+            $this->initComponents();
+            $this->getToolbarWidget()->init();
+        }
+
     }
 
-    protected function initComponents()
-    {
-        foreach ($this->_proxyMap as $name=>$class)
-        {
+    protected function initComponents() {
+        foreach ($this->_proxyMap as $name => $class) {
             $instance = Yii::app()->getComponent($name);
-            if (null !== ($instance))
-            {
+            if (null !== ($instance)) {
                 Yii::app()->setComponent($name, null);
             }
+
             $this->_proxyMap[$name] = array(
-                'class'=>$class,
+                'class' => $class,
                 'instance' => $instance
             );
         }
+
         Yii::app()->setComponents($this->_proxyMap, false);
     }
-
 
     /**
      * Processes the current request.
      * It first resolves the request into controller and action,
      * and then creates the controller to perform the action.
      */
-    private function processRequest()
-    {
-        if(is_array(Yii::app()->catchAllRequest) && isset(Yii::app()->catchAllRequest[0]))
-        {
-            $route=Yii::app()->catchAllRequest[0];
-            foreach(array_splice(Yii::app()->catchAllRequest,1) as $name=>$value)
-                $_GET[$name]=$value;
+    private function processRequest() {
+        if (is_array(Yii::app()->catchAllRequest) && isset(Yii::app()->catchAllRequest[0])) {
+            $route = Yii::app()->catchAllRequest[0];
+            foreach (array_splice(Yii::app()->catchAllRequest, 1) as $name => $value)
+                $_GET[$name] = $value;
+        } else {
+            $route = Yii::app()->getUrlManager()->parseUrl(Yii::app()->getRequest());
         }
-        else
-            $route=Yii::app()->getUrlManager()->parseUrl(Yii::app()->getRequest());
+
         Yii::app()->runController($route);
     }
 
-    protected function onEndRequest(CEvent $event)
-    {
-
-    }
-
-    public function collectLogs($logger, $processLogs=false)
-    {
-        parent::collectLogs($logger, $processLogs);
-    }
-
-    protected function processLogs($logs)
-    {
+    protected function onEndRequest(CEvent $event) {
         $this->_endTime = microtime(true);
-        $this->enabled && $this->getToolbarWidget()->run();
+    }
+
+    public function collectLogs($logger, $processLogs = false) {
+        $logs = $logger->getLogs();
+        $this->logs = empty($this->logs) ? $logs : array_merge($this->logs, $logs);
+        $this->processLogs($this->logs);
+        $this->logs = array();
+    }
+
+    protected function processLogs($logs) {
+        $this->_endTime = microtime(true);
+
+        // disable log route based on white list
+
+        $this->enabled = $this->enabled && $this->checkContentTypeWhitelist();
+
+        if ($this->enabled) {
+            $this->getToolbarWidget()->run();
+        }
+    }
+
+    private function checkContentTypeWhitelist() {
+        $contentType = '';
+
+        foreach (headers_list() as $header) {
+            list($key, $value) = explode(':', $header);
+            $value = ltrim($value, ' ');
+            if (strtolower($key) === 'content-type') {
+                // Split encoding if exists
+                $contentType = explode(";", strtolower($value));
+                $contentType = current($contentType);
+                break;
+            }
+        }
+
+        return in_array($contentType, $this->contentTypeWhitelist);
     }
 
     /**
@@ -202,30 +206,24 @@ class YiiDebugToolbarRoute extends CLogRoute
      * @param string $ip the user IP
      * @return boolean whether the user IP is allowed by {@link ipFilters}.
      */
-    protected function allowIp($ip)
-    {
-        foreach ($this->ipFilters as $filter)
-        {
+    protected function allowIp($ip) {
+        foreach ($this->ipFilters as $filter) {
             $filter = trim($filter);
             // normal or incomplete IPv4
             if (preg_match('/^[\d\.]*\*?$/', $filter)) {
                 $filter = rtrim($filter, '*');
-                if (strncmp($ip, $filter, strlen($filter)) === 0)
-                {
+                if (strncmp($ip, $filter, strlen($filter)) === 0) {
                     return true;
                 }
             }
             // CIDR
-            else if (preg_match('/^([\d\.]+)\/(\d+)$/', $filter, $match))
-            {
-                if (self::matchIpMask($ip, $match[1], $match[2]))
-                {
+            else if (preg_match('/^([\d\.]+)\/(\d+)$/', $filter, $match)) {
+                if (self::matchIpMask($ip, $match[1], $match[2])) {
                     return true;
                 }
             }
             // IPv6
-            else if ($ip === $filter)
-            {
+            else if ($ip === $filter) {
                 return true;
             }
         }
@@ -239,22 +237,19 @@ class YiiDebugToolbarRoute extends CLogRoute
      * @param integer|string $matchIp Radical of the mask (e.g. 192.168.0.0).
      * @param integer $maskBits Size of the mask (e.g. 24).
      */
-    protected static function matchIpMask($ip, $maskIp, $maskBits)
-    {
-        $mask =~ (pow(2, 32-$maskBits)-1);
-        if (false === is_int($ip))
-        {
+    protected static function matchIpMask($ip, $maskIp, $maskBits) {
+        $mask = ~ (pow(2, 32 - $maskBits) - 1);
+        if (false === is_int($ip)) {
             $ip = ip2long($ip);
         }
-        if (false === is_int($maskIp))
-        {
+        if (false === is_int($maskIp)) {
             $maskIp = ip2long($maskIp);
         }
-        if (($ip & $mask) === ($maskIp & $mask))
-        {
+        if (($ip & $mask) === ($maskIp & $mask)) {
             return true;
         } else {
             return false;
         }
     }
+
 }
